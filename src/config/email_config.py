de962df4eddb2ae3490a499b4e31f76f32b779cd
Accelerator-poi <80801261+Accelerator-poi@users.noreply.github.com>
2024-10-09 20:46:30 +0800

    Update email_config.py

--- src/config/email_config.py
+++ src/config/email_config.py
@@ -1,9 +1,4 @@
-emailconfig = {
-    "smtpHost" : "mail.cgs.gov.cn" ,
-    "user" : "agrs" ,
-    "password" : "Hangyao@60080" ,
-    "from_email" : "agrs@mail.cgs.gov.cn"
-}
+
 
 # emailconfig = {
 #     "smtpHost" : "smtp.163.com" ,

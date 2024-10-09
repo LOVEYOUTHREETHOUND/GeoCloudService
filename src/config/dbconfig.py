8effce9c80deca2eba867180680c652927c76039
Luo_yufan <2435184615@qq.com>
2024-10-09 21:01:11 +0800

    修复了已知的bug

--- src/config/dbconfig.py
+++ src/config/dbconfig.py
@@ -1,20 +1,5 @@
 # 此配置用于腾讯云服务器
-database = {
-    'user': 'JGF_GXFW',
-    'password' : 'JGF_GXFW',
-    'host' : '62.234.192.247',
-    'database' : 'ORCLCDB',
-    'port' : '18881'
-}
 
-# 此配置用于Windows Server
-# database = {
-#     'user': 'jgf_gxfw',
-#     'password' : 'icw3kx45',
-#     'host' : '10.82.8.4',
-#     'database' : 'satdb',
-#     'port' : '1521'
-# }
 
 PoolDB = {
     'mincached' : 5,

c8ab8222f884225d6c04729d9a7c0099f183b214
Luo_yufan <2435184615@qq.com>
2024-11-01 16:34:49 +0800

    修正了master上的部分错误

--- src/utils/db/oracle.py
+++ src/utils/db/oracle.py
@@ -1,5 +1,5 @@
-# import oracledb
-# import src.config.config_template as config 
+import oracledb
+import src.config.config as config 
 
 # #oracledb.init_oracle_client()
 
@@ -13,18 +13,18 @@ min = config.DB_POOL_MIN
 increment = config.DB_POOL_INCREMENT
 
 
-# def create_dbconn():
-#     str = username + '/' + password + '@' + host + ':' + port + '/' + database
-#     conn = oracledb.connect(str)
-#     return conn
+def create_dbconn():
+    str = username + '/' + password + '@' + host + ':' + port + '/' + database
+    conn = oracledb.connect(str)
+    return conn
 
-# def create_pool():
-#     # 若使用服务名连接数据库，使用下面的语句
-#     dsn = oracledb.makedsn(host, port, service_name=database)
-#     # 若使用SID连接数据库，使用下面的语句
-#     # dsn = oracledb.makedsn(host, port, sid=database)
-#     pool = oracledb.create_pool(user=username, password=password, dsn=dsn,
-#                     min=min, max=max, increment=increment)
-#     return pool
+def create_pool():
+    # 若使用服务名连接数据库，使用下面的语句
+    dsn = oracledb.makedsn(host, port, service_name=database)
+    # 若使用SID连接数据库，使用下面的语句
+    # dsn = oracledb.makedsn(host, port, sid=database)
+    pool = oracledb.create_pool(user=username, password=password, dsn=dsn,
+                    min=min, max=max, increment=increment)
+    return pool
 
 # # Mypool = create_pool()

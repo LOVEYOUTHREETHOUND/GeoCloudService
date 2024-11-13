abf2dd570056661ef72c0b54facf3dd4ed516d92
Luo_yufan <2435184615@qq.com>
2024-11-13 10:34:07 +0800

    1.重构了一键推荐 2.初步完成了检索功能核心代码

--- src/geocloudservice/recommend.py
+++ src/geocloudservice/recommend.py
@@ -1,61 +1,181 @@
 from src.utils.GeoProcessor import GeoProcessor
 from src.utils.GeoDBHandler import GeoDBHandler
 from src.utils.logger import logger
-from src.utils.db.oracle import create_pool
 
-def recommend(tablename: list, wkt: None, areacode: None) :
+def fetchDataFromDB(pool, sql:str ,param=None):
+    """从数据库中获取数据和字段名"""
+    try:
+        with pool.acquire() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, param)
+                # 默认最后一个字段名是几何字段，舍弃
+                columns = [desc[0] for desc in cur.description[:-1]]
+                data = cur.fetchall()
+        return data, columns
+    except Exception as e:
+        logger.error(f'从数据库中获取数据失败: {e}, sql: {sql}, param: {param}')
+        return None, None
+
+def getTargetArea(geodbhandler: GeoDBHandler, wkt: str, areaCode: str, pool):
+    """根据传入的wkt和行政区划代码获取目标区域的几何形状""" 
+    try:  
+        if wkt is not None:
+            target_area = geodbhandler.wktToShapely(wkt)
+        elif areaCode is not None:
+            target_area = getShapelyAreaByCode(areaCode, pool)
+        return target_area
+    except Exception as e:
+        logger.error(f'获取目标区域失败: {e}')
+        return None
+
+def recommendData(tablename: list, wkt: str, areacode: str , pool) ->list:
+    """一键推荐功能具体实现
+
+    Args:
+        tablename (list): 需要查询的表名列表(与卫星绑定)
+        wkt (str): 检索区域的wkt
+        areacode (str): 检索区域的行政区划代码
+        pool (_type_): 数据库连接池
+        areacode和wkt能且只能有一个不为空
+
+    Returns:
+        字典列表, 每一条字典代表一条数据
+    """
+    
     if wkt is None and areacode is None:
         logger.error('wkt和areacode不能同时为空')
         return None
-    pool = create_pool()
-    tableSqlList = []
-    for table in tablename:
-        table = table.upper()
-        tableSqlList.append(f'select "F_DATANAME","F_DID","F_SCENEROW","F_LOCATION","F_PRODUCTID","F_PRODUCTLEVEL",\
-            "F_CLOUDPERCENT","F_TABLENAME","F_DATATYPENAME","F_ORBITID","F_DATANAME","F_PRODUCETIME","F_SENSORID",\
-                "F_DATASIZE","F_RECEIVETIME","F_DATAID","F_SATELLITEID","F_SCENEPATH" FROM {table} ')
-    sql = ' UNION ALL '.join(tableSqlList)
+    dataname = ["F_DATANAME", "F_DID", "F_SCENEROW", "F_LOCATION", "F_PRODUCTID", "F_PRODUCTLEVEL",
+                "F_CLOUDPERCENT", "F_TABLENAME", "F_DATATYPENAME", "F_ORBITID", "F_DATANAME", "F_PRODUCETIME",
+                "F_SENSORID", "F_DATASIZE", "F_RECEIVETIME", "F_DATAID", "F_SATELLITEID", "F_SCENEPATH","F_SPATIAL_INFO"]
+    selectSql = generateSqlQuery(dataname, tablename)
+    ordersql = ' ORDER BY "F_RECEIVETIME" FETCH FIRST :limit_num ROWS ONLY'
+    sql = f'{selectSql} {ordersql}'
     geodbhandler = GeoDBHandler()
     geoprocessor = GeoProcessor()
-    sql = f'{sql} ORDER BY "F_RECEIVETIME" FETCH FIRST :limit_num ROWS ONLY'
     coverage_ratio = 0
     n = 1
-    while coverage_ratio < 0.9 and n < 9:
-        print(f'第{n}次查询')
-        limit_num = 100 * n
+    try:
+        while coverage_ratio < 0.9 and n < 9:
+            # print(f'第{n}次查询')
+            limit_num = 1000 * n
+            data, columns = fetchDataFromDB(pool, sql, {'limit_num': limit_num})
+            data_gdf = geodbhandler.dbDataToGeoDataFrame(data, columns)
+            target_area = getTargetArea(geodbhandler, wkt, areacode, pool)
+            intersected_data = geoprocessor.findIntersectedData(target_area, data_gdf)
+            coverage_ratio = geoprocessor.calCoverageRatio(target_area, intersected_data)
+            n += 1
+        result = geoprocessor.GeoDataFrameToDict(intersected_data)
+        formatted_result = formatDictForView(result)
+        return formatted_result
+    except Exception as e:
+        logger.error(f'推荐数据失败: {e}')
+        return None
+
+def searchData(tablename: list, wkt :str, areacode : str, startTime: str, endTime: str, cloudPercent: str, pool) ->list:
+    """检索功能具体实现
+
+    Args:
+        tablename (list): 需要查询的数据表名
+        wkt (str): 检索区域的wkt
+        areacode (str): 检索区域的行政区划代码
+        startTime (str): 影像数据开始时间
+        endTime (str): 影像数据结束时间
+        cloudPercent (str): 云量
+
+    Returns:
+        list: 字典列表, 每一条字典代表一条数据
+    """
+    try:
+        if wkt is None and areacode is None:
+            logger.error('wkt和areacode不能同时为空')
+            return None
+        dataname = ["F_DATANAME", "F_DID", "F_SCENEROW", "F_LOCATION", "F_PRODUCTID", "F_PRODUCTLEVEL",
+                    "F_CLOUDPERCENT", "F_TABLENAME", "F_DATATYPENAME", "F_ORBITID", "F_DATANAME", "F_PRODUCETIME",
+                    "F_SENSORID", "F_DATASIZE", "F_RECEIVETIME", "F_DATAID", "F_SATELLITEID", "F_SCENEPATH","F_SPATIAL_INFO"]
+        selectSql = generateSqlQuery(dataname, tablename)
+        whereSql = ' WHERE  F_RECEIVETIME BETWEEN TO_DATE(:startTime, \'YYYY-MM-DD HH24:MI:SS\') AND TO_DATE(:endTime, \'YYYY-MM-DD HH24:MI:SS\') AND F_CLOUDPERCENT <= :cloudPercent'
+        orderSql = ' ORDER BY "F_RECEIVETIME" FETCH FIRST :limit_num ROWS ONLY'
+        sql = f'{selectSql} {whereSql} {orderSql}'
+        ImageInfo, columns = fetchDataFromDB(pool, sql, {'startTime': startTime, 'endTime': endTime, 'cloudPercent': cloudPercent, 'limit_num': 1000})
+        geodbhandler = GeoDBHandler()
+        ImageGdf = geodbhandler.dbDataToGeoDataFrame(ImageInfo, columns)
+        target_area = getTargetArea(geodbhandler, wkt, areacode, pool)
+        geoprocessor = GeoProcessor()
+        intersected_data = geoprocessor.findIntersectedData(target_area, ImageGdf)
+        result = geoprocessor.GeoDataFrameToDict(intersected_data)
+        formatted_result = formatDictForView(result)
+        return formatted_result
+    except Exception as e:
+        logger.error(f'检索数据失败: {e}')
+        return None
+    
+       
+def getShapelyAreaByCode(areacode: str, pool): 
+    """根据传入的行政区划代码获取行政区划的几何形状
+
+    Args:
+        areacode (str): 行政区划的代码
+        pool (_type_): 数据库连接池
+
+    Returns:
+        shapely.geometry对象: 行政区划的几何形状
+    """
+    try:    
+        sql = 'select "GEOM" from TC_DISTRICT where F_DISTCODE = :areacode'
         with pool.acquire() as conn:
             with conn.cursor() as cur:
-                cur.execute(sql, {'limit_num': limit_num})
-                columns = [desc[0] for desc in cur.description[:-1]]
-                data = cur.fetchall()
-        data_gdf = geodbhandler.dbDataToGeoDataFrame(data, columns)
-        if wkt is not None:
-            target_area = geodbhandler.wktToShapely(wkt)
-        elif areacode is not None:
-            target_area = getShapelyAreaByCode(areacode, pool)
-        intersected_data = geoprocessor.findIntersectedData(target_area, data_gdf)
-        coverage_ratio = geoprocessor.calCoverageRatio(target_area, intersected_data)
-        n +=1
-    return formatDictForView(geoprocessor.GeoDataFrameToDict(intersected_data))
-        
-def getShapelyAreaByCode(areacode: str, pool): 
-    sql = 'select "GEOM" from TC_DISTRICT where F_DISTCODE = :areacode'
-    with pool.acquire() as conn:
-        with conn.cursor() as cur:
-            cur.execute(sql, {'areacode': areacode})
-            res = cur.fetchall()[0][0]
-    geodbhandler = GeoDBHandler()
-    return geodbhandler.sdoGeometryToShapely(res) 
+                cur.execute(sql, {'areacode': areacode})
+                res = cur.fetchall()[0][0]
+        geodbhandler = GeoDBHandler()
+        return geodbhandler.sdoGeometryToShapely(res)
+    except Exception as e:
+        logger.error(f'无法从areacode获取对应几何形状: {e}')
+        return None 
 
 def formatDictForView(dictList: list):
-    res = []
-    for index, data in enumerate(dictList):
-        newDict = data.copy()
-        
-        if 'geometry' in newDict.keys():
-            newDict['WKTRESPONSE'] = newDict.pop('geometry')
+    """将字典列表中的键值对进行处理, 使其适合前端展示
 
-        newDict['NODEID'] = ''
-        newDict['RN'] = index + 1
-        res.append(newDict)
-    return res
+    Args:
+        dictList (list): 字典列表
+
+    Returns:
+        list: 处理后的字典列表
+    """
+    try:
+        res = []
+        for index, data in enumerate(dictList):
+            newDict = data.copy()
+            
+            if 'geometry' in newDict.keys():
+                newDict['WKTRESPONSE'] = newDict.pop('geometry')
+
+            newDict['NODEID'] = ''
+            newDict['RN'] = index + 1
+            res.append(newDict)
+        return res
+    except Exception as e:
+        logger.error(f'格式化字典列表失败: {e}')
+        return None
+
+def generateSqlQuery(dataname: list, tablename: list) ->str:
+    """根据传入的表名和字段名生成查询语句
+
+    Args:
+        dataname (list): 传入的字段名列表
+        tablename (list): 传入的表名列表
+
+    Returns:
+        str: 查询语句
+    """
+    try:
+        tableSqlList = []
+        for table in tablename:
+            table = table.upper()
+            columns = ','.join([f'"{name}"' for name in dataname])
+            tableSqlList.append(f'select {columns} FROM {table}')
+        sql = ' UNION ALL '.join(tableSqlList)
+        return sql
+    except Exception as e:
+        logger.error(f'查询语句生成失败: {e}')
+        return None
--- src/utils/GeoDBHandler.py
+++ src/utils/GeoDBHandler.py
@@ -8,19 +8,24 @@ class GeoDBHandler:
         self.crs = config.CRS
         
     def wktToShapely(self, wkt: str) -> gpd.GeoDataFrame:
-        """
-        将wkt转成Shapeley对象;
-        wkt: WKT格式的几何形状;
+        """将wkt转成Shapeley对象;
+        Args:
+            wkt: WKT格式的几何形状;
+        Returns:
+            gpd.GeoDataFrame;
         """
         geometry = shapely.wkt.loads(wkt)
         return geometry
         
     def dbDataToGeoDataFrame(self, rows, columns) -> gpd.GeoDataFrame:
-        """
-        将数据库查询结果转换为GeoDataFrame;
-        rows: 数据库查询结果;
-        columns: 除几何数据外的列名;
-        默认最后一列为SDO_GEOMETRY对象;
+        """将数据库查询结果转换为GeoDataFrame;
+        Args:
+        
+            rows: 数据库查询结果;
+            columns: 除几何数据外的列名;
+            默认最后一列为SDO_GEOMETRY对象;
+        Returns:
+            gpd.GeoDataFrame;
         """
         geometries = []
         attributes = []
@@ -36,12 +41,15 @@ class GeoDBHandler:
         return gdf
 
     def sdoGeometryToGeoDataFrame(self, sdo_geometry, additional_columns=None, additional_data=None) -> gpd.GeoDataFrame:
-        """
-        将SDO_GEOMETRY对象以及其余可能需要的属性转换为GeoDataFrame;
-        sdo_geometry: SDO_GEOMETRY对象,一般为从数据库中读取的数据;
-        additional_columns: 其余数据的列名,如F_ID, NAME等;
-        additional_data: 其余数据;
-        注意additional_data和additional_columns的列数应该相等,且其行数与sdo_geometry的行数相等;
+        """将SDO_GEOMETRY对象以及其余可能需要的属性转换为GeoDataFrame;
+        
+        Args:
+            sdo_geometry: SDO_GEOMETRY对象,一般为从数据库中读取的数据;
+            additional_columns: 其余数据的列名,如F_ID, NAME等;
+            additional_data: 其余数据;
+            注意additional_data和additional_columns的列数应该相等,且其行数与sdo_geometry的行数相等;
+        Returns:
+            gpd.GeoDataFrame;
         """
         try:
             geometries = []
@@ -68,10 +76,14 @@ class GeoDBHandler:
             
     
     def sdoGeometryToShapely(self, sdo_geometry):    
-        """
-        将SDO_GEOMETRY对象转换为shapely几何对象。
+        """将SDO_GEOMETRY对象转换为shapely几何对象。
+        
+        Args:
         sdo_geometry: SDO_GEOMETRY对象(从数据库中直接读取);
+        Returns:
+        shapely几何对象;
         """
+        
         if sdo_geometry is None:
             return None
 
@@ -86,14 +98,15 @@ class GeoDBHandler:
         elif sdo_gtype == 2003:  # 多边形
             polygon = shapely.geometry.Polygon(self.pairwise(sdo_ordinates))
             return polygon
-        # 可以根据需要处理其他几何类型
+        elif sdo_gtype == 2007: # 多点
+            multipoint = shapely.geometry.MultiPoint(self.pairwise(sdo_ordinates))
+            return multipoint
         else:
-            logger.error(f"Unsupported SDO_GTYPE: {sdo_gtype}")
+            logger.error(f"不支持的类型 SDO_GTYPE: {sdo_gtype}")
             return None
         
     def pairwise(self, iterable):
-        """
-        将可迭代对象两两配对;
+        """将可迭代对象两两配对;
         [long_1, lat_1, long_2, lat_2 ... long_5, lat_5] -> [(long_1, lat_1), (long_2, lat_2)...]
         """
         return [(iterable[i], iterable[i + 1]) for i in range(0, len(iterable)-1, 2)]
